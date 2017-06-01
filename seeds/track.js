1c2117b0bf87afdaae9a6c104aeffb047f56fc96
Shannon Rivers <shannon.rivers@gmail.com>
2017-06-01 15:20:06 -0600

    complete script

--- seeds/track.js
+++ seeds/track.js
@@ -1,11 +1,10 @@
-
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
     knex('track').del(),
 
     // Inserts seed entries
-    knex('track').insert({id: 1, name: 'Strawberry Fields'}),
-    knex('track').insert({id: 2, name: 'Soft Bulletin'})
+    knex('track').insert({id: 1, name: 'Strawberry Fields', artist_id: 1}),
+    knex('track').insert({id: 2, name: 'Soft Bulletin', artist_id: 2})
   );
 };
